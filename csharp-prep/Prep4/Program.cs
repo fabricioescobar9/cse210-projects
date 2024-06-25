72d7cb6bbbd447430de45cdbcabf546a582af489
fabricioescobar9 <esc24001@byui.edu>
2024-06-25 01:16:32 -0300

    C# Prep 4 - Finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,73 @@
 using System;
+using System.Globalization;
+using System.Collections.Generic; 
+
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while(userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+
+            string numberInput = Console.ReadLine();
+            userNumber = int.Parse(numberInput);
+
+            if(userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        int sumNumbers = 0;
+
+        foreach (int number in numbers)
+        {
+            sumNumbers += number; 
+        }
+
+        Console.WriteLine($"The sum is: {sumNumbers}");
+
+        float averageNumbers = ((float)sumNumbers) / numbers.Count;
+
+        Console.WriteLine($"The average is: {averageNumbers}");
+
+        int maxNumbers = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxNumbers)
+            {
+                maxNumbers = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {maxNumbers}");
+
+        int minNumbers = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < minNumbers && number > 0)
+            {
+                minNumbers = number;
+            }
+        }
+
+        Console.WriteLine($"The min positive is: {minNumbers}");
+
+        Console.WriteLine("The sorted list is: ");
+        numbers.Sort();
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
     }
 }
