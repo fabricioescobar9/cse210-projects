d2f526a5a8ba90eb9c90257be701e9c6d8c08042
fabricioescobar9 <esc24001@byui.edu>
2024-06-24 23:10:43 -0300

    C# Prep 3 - Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+    {  
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            int guess = 0;
+            int guessCount = 0;
+
+            while (number != guess)
+            {
+                Console.Write("What is your guess? ");
+                string guessInput = Console.ReadLine();
+                guess = int.Parse(guessInput);  
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                    guessCount++; 
+                }
+
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                    guessCount++; 
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessCount++; 
+                }
+            }
+
+            Console.WriteLine($"It took {guessCount} guesses");
+
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+            Console.WriteLine();
+        }
+    }   
 }
